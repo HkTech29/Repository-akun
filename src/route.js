74a33dc3a7ec1d1afaf8d9560e01f162fe242a4e
Hendra <78556460+HkTech29@users.noreply.github.com>
2024-08-10 00:16:44 +0700

    change branch name

--- src/route.js
+++ src/route.js
@@ -5,9 +5,44 @@ const admin = require('firebase-admin');
 
 
 async function routeHandler(server) {
-    const db = admin.firestore();
-<<<<<<< HEAD
+    const db = admin.firestore(); 
     server.route({
+        method: 'POST',
+        path: '/register',
+        handler: async (request, h) => {
+            const { name, email, password } = request.payload;
+            const db = admin.firestore();
+    
+            const schema = Joi.object({
+                name: Joi.string().min(3).max(20).required(),
+                email: Joi.string().email().required(),
+                password: Joi.string().min(8).required()
+            });
+    
+            const { error } = schema.validate(request.payload, { abortEarly: false });
+            if (error) {
+                const translatedErrors = error.details.map(err => translateJoiError(err));
+                return h.response({ error: true, message: translatedErrors.join(', ') }).code(400);
+            }
+    
+            const userSnapshot = await db.collection('users').where('email', '==', email).get();
+            if (!userSnapshot.empty) {
+                return h.response({ error: true, message: 'Email sudah digunakan, silakan coba lagi' }).code(400);
+            }
+    
+            const hashedPassword = await argon2.hash(password);
+    
+            const userRef = db.collection('users').doc();
+            await userRef.set({
+                name,
+                email,
+                password: hashedPassword
+            });
+    
+            return h.response({ error: false, message: 'Pengguna berhasil dibuat' }).code(201);
+        }
+    });
+     server.route({
         method: 'POST',
         path: '/login',
         handler: async (request, h) => {
@@ -43,7 +78,7 @@ async function routeHandler(server) {
             }).code(200);
         }
     });
-
+  
 }
 
 module.exports = routeHandler;
